528a71b5d82bd23872bd52b9616238c92073ca7c
youjin kim <yujin5836@gmail.com>
2024-02-22 16:43:43 +0900

    Update : address
    update hacker add

--- apigraph.py
+++ apigraph.py
@@ -9,11 +9,14 @@ from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 
 BLOCKCHAIN_API_BASE = 'https://blockchain.info'
 
 DATA_FILE_PATH = '/home/ujin/Desktop/bitcoin/bit_trace/sextortion_addresses.csv'
+# 한글 폰트 설정
+plt.rcParams['font.family'] = 'NanumBarunGothic'  # 예: 'NanumBarunGothic' 등 한글 폰트 이름으로 변경
 
 
 def load_hackers_data(file_path):
@@ -55,13 +58,17 @@ def get_transactions(hacker_address, report_type):
     G = nx.DiGraph()  # 방향성 그래프 생성
     delay = 30
     max_delay = 60
-    hacker_addresses_queue = [hacker_address]
+    hacker_addresses_queue = [(hacker_address, 0)]  # 초기 깊이 0으로 설정
     processed_addresses = set([hacker_address])  # 초기 해커 주소를 처리된 주소에 추가
     output_filename = f"{report_type}.Transaction_{hacker_address}.csv"  # 파일 이름 설정
     offset = 0
 
     while hacker_addresses_queue:
-        current_hacker_address = hacker_addresses_queue.pop(0)
+        current_hacker_address, current_depth = hacker_addresses_queue.pop(0)
+        if current_depth > 10:  # 재귀 깊이가 10을 초과하는 경우 종료
+            print("최대 재귀 깊이 초과")
+            break
+
         retry_count = 3
 
         while retry_count > 0:
@@ -79,49 +86,49 @@ def get_transactions(hacker_address, report_type):
                 time.sleep(delay)
                 delay = min(delay * 2, max_delay)
 
-        if response.status_code == 200:
-            if 'txs' in data:
-                for tx in data['txs']:
-                    for output in tx['out']:
-                        if 'addr' in output:
-                            receiving_wallet = output['addr']
-                            transaction_amount = output['value'] / 1e8
-                            
-                            # 트랜잭션 데이터 구성
-                            transaction_data = {
-                                'tx_hash': tx['hash'],
-                                'sending_wallet': current_hacker_address,
-                                'receiving_wallet': receiving_wallet,
-                                'transaction_amount': transaction_amount,
-                                'coin_type': 'BTC',
-                                'date_sent': datetime.fromtimestamp(tx['time']).strftime('%Y-%m-%d'),
-                                'time_sent': datetime.fromtimestamp(tx['time']).strftime('%H:%M:%S')
-                            }
-                            
-                            # 파일에 트랜잭션 데이터 기록
-                            write_transaction_to_file(transaction_data, output_filename)
+        if response.status_code == 200 and 'txs' in data:
+            if data.get('n_tx', 0) >= 3000:  # 수취인 주소가 3000개 이상의 트랜잭션을 가지고 있는 경우 종료
+                print(f"슈퍼 노드 발견: {current_hacker_address}")
+                break
 
-                            # 그래프에 노드와 에지 추가
-                            G.add_node(current_hacker_address)
-                            G.add_node(receiving_wallet)
-                            if G.has_edge(current_hacker_address, receiving_wallet):
-                                G[current_hacker_address][receiving_wallet]['weight'] += transaction_amount
-                            else:
-                                G.add_edge(current_hacker_address, receiving_wallet, weight=transaction_amount)
+            for tx in data['txs']:
+                for output in tx['out']:
+                    if 'addr' in output:
+                        receiving_wallet = output['addr']
+                        transaction_amount = output['value'] / 1e8
+                        
+                        # 트랜잭션 데이터 구성 및 파일에 기록
+                        transaction_data = {
+                            'tx_hash': tx['hash'],
+                            'sending_wallet': current_hacker_address,
+                            'receiving_wallet': receiving_wallet,
+                            'transaction_amount': transaction_amount,
+                            'coin_type': 'BTC',
+                            'date_sent': datetime.fromtimestamp(tx['time']).strftime('%Y-%m-%d'),
+                            'time_sent': datetime.fromtimestamp(tx['time']).strftime('%H:%M:%S')
+                        }
+                        write_transaction_to_file(transaction_data, output_filename)
 
-                            if receiving_wallet not in processed_addresses:
-                                hacker_addresses_queue.append(receiving_wallet)
-                                processed_addresses.add(receiving_wallet)
+                        # 그래프에 노드와 에지 추가
+                        G.add_node(current_hacker_address)
+                        G.add_node(receiving_wallet)
+                        if G.has_edge(current_hacker_address, receiving_wallet):
+                            G[current_hacker_address][receiving_wallet]['weight'] += transaction_amount
+                        else:
+                            G.add_edge(current_hacker_address, receiving_wallet, weight=transaction_amount)
 
-                offset += 50  # 다음 트랜잭션 페이지로 이동
-            else:
-                break  # 트랜잭션 데이터가 없는 경우 종료
+                        if receiving_wallet not in processed_addresses:
+                            hacker_addresses_queue.append((receiving_wallet, current_depth + 1))
+                            processed_addresses.add(receiving_wallet)
+
+            offset += 50  # 다음 트랜잭션 페이지로 이동
         else:
             if response.status_code == 429:  # 요청 제한에 도달한 경우
                 time.sleep(delay)
         time.sleep(delay + random.uniform(0, 3))  # API 요청 간 지연 시간
 
     return G  # 분석 완료된 그래프 반환
+
 
 def get_next_hacker_address(transactions):
     if transactions:
